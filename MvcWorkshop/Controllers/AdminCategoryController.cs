201cfa03507d3eee9b41226624663b55b8e8a873
seckinsoygan <seckinsoygann@gmail.com>
2023-06-14 18:30:52 +0300

    Category part added in new theme interface

--- MvcWorkshop/Controllers/AdminCategoryController.cs
+++ MvcWorkshop/Controllers/AdminCategoryController.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcWorkshop.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
         public IActionResult Index()
         {
+            var categoryValues = cm.GetAll();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator validator = new CategoryValidator();
+            ValidationResult Result = validator.Validate(category);
+            if (Result.IsValid)
+            {
+                cm.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in Result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
